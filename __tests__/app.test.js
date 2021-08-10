1b3eab00572222e4612d3c74b86b27ea9ad6d02d
David Delgadillo <daviddel.731@gmail.com>
2021-08-10 12:54:04 -0700

    added get by id route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Player from '../lib/models/Player.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -13,6 +14,7 @@ describe('demo routes', () => {
       .post('/api/v1/players')
       .send({
         name: 'leBron',
+        image: 'lebron.png',
         years_played: '2003 - present',
         championships_won: 4,
         total_points: 38000,
@@ -24,6 +26,7 @@ describe('demo routes', () => {
     expect(res.body).toEqual(
       { id: '1',
         name: 'leBron',
+        image: 'lebron.png',
         years_played: '2003 - present',
         championships_won: 4,
         total_points: '38000',
@@ -31,5 +34,27 @@ describe('demo routes', () => {
         blocks: 4000,
         assists: 3800 }
     );
+  });
+
+  it('should get a player by id via GET', async () => {
+    
+
+    const player = {
+      id: '1',
+      name: 'leBron',
+      image: 'lebron.png',
+      years_played: '2003 - present',
+      championships_won: 4,
+      total_points: 38000,
+      fg_percentage: 55.90,
+      blocks: 4000,
+      assists: 3800
+    };
+
+    const player1 = await Player.insert(player);
+
+    const res = await request(app).get(`/api/v1/players/${player1.id}`);
+
+    expect(res.body).toEqual(player1);
   });
 });
